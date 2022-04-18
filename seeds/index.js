822908e327b8dd6d12713ef2a6b21bb85f5976f4
George991808 <gellis7@gmail.com>
2022-04-18 21:53:22 +0800

    thought model created

--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,12 @@
 const seedUser = require("./user-seed");
+const seedThought = require("./thought-seed");
 const db = require("./../config/connection");
 const seedAll = async () => {
   await seedUser(5);
   console.log("user seeded");
+
+  await seedThought(5);
+  console.log("thought seeded");
   process.exit(0);
 };
 db.once("open", () => {
---
+++ seeds/thought-seed.js
@@ -0,0 +1,18 @@
+const Thought = require("../models/Thought");
+const { faker } = require("@faker-js/faker");
+const User = require("../models/User");
+async function seedThought(numbers) {
+  await Thought.deleteMany();
+  const thoughts = [];
+  for (let i = 0; i < numbers; i++) {
+    const randomUser = (await User.aggregate([{ $sample: { size: 1 } }]))[0];
+    const created = await Thought.create({
+      thoughtText: faker.lorem.sentence(5),
+      username: randomUser,
+    });
+    thoughts.push(created);
+  }
+
+  return thoughts;
+}
+module.exports = seedThought;
