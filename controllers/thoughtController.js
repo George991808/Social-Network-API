f2c5223ddb25b653320ac365a9b127ea5bafeea0
George991808 <gellis7@gmail.com>
2022-04-18 22:49:03 +0800

    thought routes working need to add link to user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -17,7 +17,7 @@ const grade = async (thoughtId) =>
       // Your code here
     },
     {
-      $unwind: "$assignments",
+      $unwind: "$reactions",
     },
     // TODO: Group information for the thought with the given ObjectId alongside an overall grade calculated using the $avg operator
     {
@@ -29,17 +29,8 @@ module.exports = {
   // Get all thoughts
   getThoughts(req, res) {
     Thought.find()
-      .then(async (thoughts) => {
-        const thoughtObj = {
-          thoughts,
-          headCount: await headCount(),
-        };
-        return res.json(thoughtObj);
-      })
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
   },
   // Get a single thought
   getSingleThought(req, res) {
@@ -51,7 +42,6 @@ module.exports = {
           ? res.status(404).json({ message: "No thought with that ID" })
           : res.json({
               thought,
-              grade: await grade(req.params.thoughtId),
             })
       )
       .catch((err) => {
@@ -89,14 +79,26 @@ module.exports = {
         res.status(500).json(err);
       });
   },
-
-  // Add an assignment to a thought
-  addAssignment(req, res) {
-    console.log("You are adding an assignment");
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Add an reaction to a thought
+  addReaction(req, res) {
+    console.log("You are adding an reaction");
     console.log(req.body);
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
-      { $addToSet: { assignments: req.body } },
+      { $addToSet: { reactions: req.body } },
       { runValidators: true, new: true }
     )
       .then((thought) =>
@@ -108,11 +110,11 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  // Remove assignment from a thought
-  removeAssignment(req, res) {
+  // Remove reaction from a thought
+  removeReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
-      { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
+      { $pull: { reaction: { reactionId: req.params.reactionId } } },
       { runValidators: true, new: true }
     )
       .then((thought) =>
