69b77930aab8c2e90ed4291f5253a2fdb3b18b1b
George991808 <gellis7@gmail.com>
2022-04-18 20:18:43 +0800

    copied e commerce and created first model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const validateEmail = function (email) {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please provide a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{
+      model: Product
+    }
+  ]
+  }).then((tags) => {
+    res.json(tags);
+  })
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [{
+      model: Product
+    }]
+    }).then((tag) => {
+      res.json(tag);
+    })
+
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  }).then((created) => {
+    res.json(created);
+  })
+
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  },{
+    where: {
+      id: req.params.id
+    }
+  }).then((updated) => {
+     res.json(updated);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    }
+  }).then((deleted) => {
+    res.json(deleted);
+  })
+});
+
+module.exports = router;
